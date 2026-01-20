483fbf12530c829f5a4f4a9d6e89865d2a23795b
Alvans <achicallag@unjbg.edu.pe>
2026-01-19 22:37:17 -0500

    agregando holas

---
+++ Basico/00-HolaMundo.js
@@ -0,0 +1,10 @@
+//comentaro
+console.log("Hola mundo")
+console.log('Hola mundo')
+console.log('Hola mundo gaaa')
+
+console.log("5")
+console.log(5)
+console.log(5+5)
+console.log(5*5)
+console.log(3+4+5-1-2-3-5-7-8-9-10)
