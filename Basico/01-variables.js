2530d35fad5d01415ced68cc8112fe5a12f82455
Alvans <achicallag@unjbg.edu.pe>
2026-01-28 18:00:33 -0500

    variables

---
+++ Basico/01-variables.js
@@ -0,0 +1,21 @@
+/*
+*/
+
+//var, no se aconseja usar
+var hola = "holas"
+console.log(hola)
+
+hola="hola 2"
+console.log(hola)
+//let , definimos variables cuando varian
+let hola2 = "HOLA2"
+console.log(hola2)
+hola2 = "HOLA2222"
+console.log(hola2)
+
+//const, definimos varialbes cuando no varian , se quedan bloqueados
+const hola3="hola3"
+console.log(hola3)
+hola3="hola4"
+console.log(hola3)
+
