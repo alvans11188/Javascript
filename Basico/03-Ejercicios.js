2a0d41aa622001cf3d0503b93318d0a1afe05d7f
Alvans <achicallag@unjbg,edu,pe>
2026-02-09 21:42:59 -0500

    ejercicios resueltos

---
+++ Basico/03-Ejercicios.js
@@ -0,0 +1,75 @@
+// 1. Escribe un comentario en una línea
+//hola :v
+// 2. Escribe un comentario en varias líneas
+/*
+hola
+hola
+*/
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+let nombre="alex" //string
+let numero=1 //numeros
+let booleanos=true //booleanos
+let undefined //indefinido
+let nullValor=null //nulo
+let simbolo=Symbol("simbolo") //simbolo
+let big= BigInt(123131231323424234234234234)
+let big2=123131231313123n 
+
+// 4. Imprime por consola el valor de todas las variables
+console.log(nombre)
+console.log(numero)
+console.log(booleanos)
+console.log(undefined)
+console.log(nullValor)
+console.log(simbolo)
+console.log(big)
+console.log(big2)
+
+console.log(nombre,numero,booleanos,undefined,nullValor,simbolo,big,big2)
+// 5. Imprime por consola el tipo de todas las variables
+console.log(typeof nombre)
+console.log(typeof numero)
+console.log(typeof booleanos)
+console.log(typeof undefined)
+console.log(typeof nullValor)
+console.log(typeof simbolo)
+console.log(typeof big)
+console.log(typeof big2)
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+nombre="juan" //string
+numero=12 //numeros
+booleanos=false //booleanos
+ //indefinido
+ //nulo
+simbolo=Symbol("simbolo2") //simbolo
+big= BigInt(32214124124)
+big2=124124214241n 
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+nombre=12
+numero="doce"
+booleanos=null
+undefined = "NO"
+nullValor=true
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+const cnombre="alex"
+const cnumero=15
+const cbooleano=false
+const cUndefined = undefined;
+const cNull = null;
+const cSymbol = Symbol("id");
+const cBigInt = 9007199254740991n;
+// 9. A continuación, modifica los valores de las constantes
+//cnombre="jaaa"
+//cnumero=18
+//cbooleano=true
+
+//cSymbol = Symbol("oky");
+//cBigInt = 9007190991n;
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
+//cnombre="jaaa"
+//cnumero=18
+//cbooleano=true
+
+//cSymbol = Symbol("oky");
+//cBigInt = 9007190991n;
